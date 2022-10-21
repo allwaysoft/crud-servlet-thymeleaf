f57d348b72e8b04089c1e631b886e716493b572b
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2022-10-21 22:31:44 +0800

    Add files via upload

--- src/main/java/com/example/crud/SQL/init_db.sql
+++ src/main/java/com/example/crud/SQL/init_db.sql
@@ -4,6 +4,6 @@ CREATE TABLE `suppliers`
     `name`        VARCHAR(500) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_unicode_ci',
     `description` VARCHAR(500) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_unicode_ci',
     PRIMARY KEY (`id`) USING BTREE
-) INSERT INTO suppliers VALUES (1,'Amazon', 'Digital content and services');
-INSERT INTO suppliers
-VALUES (2, 'Lenovo', 'Computers');
+) ;
+INSERT INTO suppliers VALUES (1,'Amazon', 'Digital content and services');
+INSERT INTO suppliers  VALUES (2, 'Lenovo', 'Computers');
--- src/main/java/com/example/crud/controller/SupplierController.java
+++ src/main/java/com/example/crud/controller/SupplierController.java
@@ -1,7 +1,6 @@
 package com.example.crud.controller;
 
 import com.example.crud.config.TemplateEngineUtil;
-import com.example.crud.dao.SupplierDao;
 import com.example.crud.implementation.SupplierDaoJDBC;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -10,32 +9,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
 public class SupplierController extends HttpServlet {
-
-    private SupplierDao productSupplierDataStore = SupplierDaoJDBC.getInstance();
-
-    private Map<String, Object> params = createMap();
-
-    public Map<String, Object> getParams() {
-        return params;
-    }
-
-    private Map<String, Object> createMap() {
-
-        params = new HashMap<String, Object>();
-        params.put("supplierList", productSupplierDataStore.getAll());
-        return params;
-    }
-
-    public SupplierDao getProductSupplierDataStore() {
-        return productSupplierDataStore;
-    }
 
     public void renderTemplate(HttpServletRequest req, HttpServletResponse resp, String HTMLpath, Map<String, Object> optionalParameters) throws IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
@@ -48,8 +27,8 @@ public class SupplierController extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        Map<String, Object> params = getParams();
-
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("supplierList", SupplierDaoJDBC.getInstance().getAll());
         renderTemplate(req, resp, "supplier.html", params);
     }
 }
